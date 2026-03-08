4440a7d37a111acb4b6387bdeb0540a7be1e37e4
anasnaeem1 <clownop473@gmail.com>
2026-03-08 09:13:37 +0500

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["framer-motion"],
+  experimental: {
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+        "node_modules/webpack/schemas",
+      ],
+    },
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
